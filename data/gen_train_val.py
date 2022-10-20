362d185b6b02a99570ba6ae8d8978bc81171e7f2
aeeeeeep <aep_sun@qq.com>
2022-10-20 16:01:50 +0800

    yolov7-e6e

--- data/gen_train_val.py
+++ data/gen_train_val.py
@@ -10,17 +10,17 @@ def gen_train_val(path, ratio):
     for img in imgs:
         if img not in train_imgs:
             val_imgs.append(img)
-    with open('./splittrain.txt', 'a', encoding='utf-8') as f:
+    with open('./train.txt', 'a', encoding='utf-8') as f:
         for train_img in train_imgs:
-            f.write(os.path.dirname(os.path.abspath(__file__))+'/train/splitimages/'+train_img+'\n')
+            f.write(os.path.dirname(os.path.abspath(__file__))+'/train/images/'+train_img+'\n')
             # f.write(train_img.split('.')[0]+ '\n')
-    with open('./splitval.txt', 'a', encoding='utf-8') as f:
+    with open('./val.txt', 'a', encoding='utf-8') as f:
         for val_img in val_imgs:
-            f.write(os.path.dirname(os.path.abspath(__file__))+'/train/splitimages/'+val_img+'\n')
+            f.write(os.path.dirname(os.path.abspath(__file__))+'/train/images/'+val_img+'\n')
             # f.write(val_img.split('.')[0]+'\n')
 
 
 if __name__ == '__main__':
-    path = './train/splitimages'
+    path = './train/images'
     ratio = 0.9
     gen_train_val(path, ratio)
